9b68d230f40bdd6889b5256b83d9e567327e68ce
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-25 00:50:49 +0530

    added ResultScreen with a LeaderrBoard and a highScore!

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
   return <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
     
     <AnimatePresence mode='wait'>
-      {phase === 'start' && <TapNowScreen key={'start'}/>}
+      {phase === 'start' && <ResultScreen key={'start'}/>}
       {phase === 'waiting' && <WaitingScreen key={'waiting'}/>}
       {phase === 'tap' && <TapNowScreen/>}
       {phase === 'result' && <ResultScreen/>}
--- src/components/ResultScreen.tsx
+++ src/components/ResultScreen.tsx
@@ -1,5 +1,21 @@
+import { motion } from 'framer-motion';
+ 
 export function ResultScreen(){
     return <div>
-        
+        <motion.div className='min-h-72 w-92  p-8 rounded-2xl shadow-2xl bg-white/10 backdrop-blur-md border border-white/20 max-w-sm mx-auto space-y-4'>
+            <h1 className='font-mono font-bold text-center mt-5 text-2xl '>⏱️ Your Reaction Time: </h1>
+            <h1 className='font-mono font-bold text-center mt-5 text-2xl text-amber-200 '>100ms</h1>
+            <HighScore/>
+        </motion.div>
+        <motion.div className='bg-white/10 rounded-2xl border border-white/20 mt-3 flex items-center p-4'>
+        <h1 className='font-mono font-bold ml-6 text-2xl py-4 '>LeaderBoard:</h1>
+        </motion.div>
+    </div>
+}
+
+
+function HighScore(){
+    return <div className='font-mono font-bold text-center mt-5 text-2xl text-amber-400'>
+        🏆 New High Score!
     </div>
 }
